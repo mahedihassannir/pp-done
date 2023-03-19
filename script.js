91b2b00ba1e82b02ca0b58b0bf331318f4342a34
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-19 14:33:41 +0600

    all ojk

---
+++ script.js
@@ -0,0 +1,123 @@
+// // how to dicler variable 
+
+
+// // lets do it
+
+
+// let mahhedi=10
+
+// let mahdi2=10
+
+// let total=mahhedi+mahdi2
+
+// console.log(total);
+
+
+let mahedi=100
+
+// how to use ternary opratore
+
+
+// let mahedi2=mahedi===100?"mahedi is good boy":"mahedi is a pro"
+// console.log(mahedi2);
+
+
+// if(mahedi===100){
+    
+//     console.log("mahedi is good boy");
+// }
+// else{
+//     console.log("mahedi is not a good boy");
+// }
+
+
+
+let mahedis=[11,22,33,44,55,66]
+
+
+// for(let i=0; i<mahedis.length; i++ ){
+//     let elemetn=mahedis[i]
+//     console.log(elemetn);
+// }
+
+// for(let mahedi of mahedis)
+
+// console.log(mahedi);
+// i=0
+
+// while(let =i <mahedis.length){
+//     let element=mahedis[i]
+
+//     i++
+//     console.log(element);
+// }
+
+
+// function fs(m,n){
+
+//   let total=m+n
+// return total
+// }
+
+// let final=fs(10,10)
+// console.log(final);
+
+
+// let mahedi23=[11,22,33,44,55]
+
+// let total=[...mahedi23]
+// total.push(100)
+// console.log(total,mahedi23);
+
+
+// let mahedi121=(m,n)=>{
+//     let total=m+n
+//     console.log(total);
+
+// }
+// mahedi121(10,10)
+
+// let total={name:"John", "age":30, "car":null}
+
+// let final=JSON.stringify(total)
+// console.log(final);
+
+
+
+
+// let loddatas=()=>{
+//     let url=``
+//     fetch(url)
+//     .then(res=>res.json())
+//     .then((data)=>console.log(data))
+    
+// }
+
+// loddatas()
+
+
+// let m=false
+
+// let total=!!m
+// // console.log(total);
+
+// let mahedi21221="111"
+
+// // convert string to number
+
+
+// let total=parseInt(mahedi21221)
+// console.log(total);
+
+// let total=mahedi21221.toString()
+// console.log(total);
+
+let mahdi2={
+    name:"mahedi",
+    kalo:"mahedi"
+}
+
+
+    let mahedi1232331231=mahdi2["name"]
+
+    console.log(mahedi1232331231);
